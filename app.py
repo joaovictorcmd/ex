45b5b582eeea1e023e91dc2ac0777f0bf700ea3e
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2024-04-04 13:53:08 -0300

    Att

--- app.py
+++ app.py
@@ -34,13 +34,9 @@ def sobre():
 @app.route("/filmes")
 def filmes():
     url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3d3ead3c05d27f6eb1d604befda8b752"
-
     resposta = urllib.request.urlopen(url)  # Request to server
-
     dados = resposta.read()  # Read the response given from server
-
     jsondata = json.loads(dados)
-
     return render_template("filmes.html", filmes=jsondata["results"])
 
 
