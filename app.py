ab515c31c62a21555b2ee617b81463f1f8b99c25
Joao Victor Carvalho Machado <machado.joaovictor@outlook.com>
2024-04-05 13:24:58 -0300

    Atualização das páginas relacionadas a cursos

--- app.py
+++ app.py
@@ -1,11 +1,27 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import urllib.request, json
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.run(debug=True)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///cursos.sqllite3"
+
+db = SQLAlchemy(app)
 
 frutas = []
 registros = []
+
+
+class cursos(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nome = db.Column(db.String(50))
+    descricao = db.Column(db.String(100))
+    ch = db.Column(db.Integer)
+
+    def __init__(self, nome, descricao, ch):
+        self.nome = nome
+        self.descricao = descricao
+        self.ch = ch
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -31,17 +47,47 @@ def sobre():
     return render_template("sobre.html", registros=registros)
 
 
-@app.route("/filmes")
-def filmes():
-    url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3d3ead3c05d27f6eb1d604befda8b752"
+@app.route("/filmes/<propriedade>")
+def filmes(propriedade):
+    if propriedade == "populares":
+        url = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=3d3ead3c05d27f6eb1d604befda8b752"
+    elif propriedade == "kids":
+        url = "https://api.themoviedb.org/3/discover/movie?certification_country=US&certification.lte=G&sort_by=popularity.desc&api_key=3d3ead3c05d27f6eb1d604befda8b752C"
+    elif propriedade == "2010":
+        url = "https://api.themoviedb.org/3/discover/movie?primary_release_year=2010&sort_by=vote_average.desc&api_key=3d3ead3c05d27f6eb1d604befda8b752C"
+    elif propriedade == "drama":
+        url = "https://api.themoviedb.org/3/discover/movie?with_genres=18&sort_by=vote_average.desc&vote_count.gte=10&api_key=3d3ead3c05d27f6eb1d604befda8b752C"
+    elif propriedade == "tom_cruise":
+        url = "https://api.themoviedb.org/3/discover/movie?with_genres=878&with_cast=500&sort_by=vote_average.desc&api_key=3d3ead3c05d27f6eb1d604befda8b752C"
     resposta = urllib.request.urlopen(url)  # Request to server
     dados = resposta.read()  # Read the response given from server
     jsondata = json.loads(dados)
     return render_template("filmes.html", filmes=jsondata["results"])
 
 
+@app.route("/cursos")
+def lista_cursos():
+    return render_template("cursos.html", cursos=cursos.query.all())
+
+
+@app.route("/criar_curso", methods=["GET", "POST"])
+def criar_curso():
+    nome = request.form.get("nome")
+    descricao = request.form.get("descricao")
+    ch = request.form.get("ch")
+
+    if request.method == "POST":
+        curso = cursos(nome, descricao, ch)
+        db.session.add(curso)
+        db.session.commit()
+        return redirect(url_for("lista_cursos"))
+    return render_template("novo_curso.html")
+
+
 # Debug mode
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
 
 # http://127.0.0.1:5000/
